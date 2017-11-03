bfc3aca0f868305cd2dae72e489e6f4635113c67
Quentin Tang <quentin.tang@softwareengineering.it>
2017-11-04 00:52:57 +0100

    added the basic file structure. Iss good.

---
+++ host/host.py
@@ -0,0 +1,9 @@
+from uuid import uuid4
+
+from flask import Flask
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
